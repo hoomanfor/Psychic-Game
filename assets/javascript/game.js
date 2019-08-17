94540e3780fb79b40dddaf5168bb283ebc72554a
Hoo Man <hoomanfor@gmail.com>
2019-08-16 20:02:28 -0400

    randomAlphabet chooses a random alphabet

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var randomAlphabet = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+console.log(randomAlphabet);
