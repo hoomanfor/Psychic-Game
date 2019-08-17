c4ad3c939658726e38f108783f99f40114511837
Hoo Man <hoomanfor@gmail.com>
2019-08-17 14:01:43 -0400

    removed logs and updated readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# The Psychic Game
+
+Check it out: [The Psychic Game](https://hoomanfor.github.io/psychic-game/)
+
+In this exercise, I have built a game using Vanilla Javascript. Once the game starts, the app will randomly select an alphabet from A to Z. Your goal is to guess this random alphabet. Per game, you have 9 guesses to win or lose. Good Luck! :-D 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var randomAlphabet = alphabet[Math.floor(Math.random() * alphabet.length)];
-console.log("randomAlphabet: " + randomAlphabet);
 
 var wins = 0;
 var losses = 0;
@@ -15,14 +14,11 @@ var domAlreadyGuessed = document.querySelector("#already-guessed");
 
 document.onkeyup = function play() {
     var guess = event.key.toLowerCase();
-    console.log("event: ", event);
-    console.log("guess: " + guess);
     if (event.keyCode >= 65 && event.keyCode <= 90 && alreadyGuessed.indexOf(guess) == -1) {
         if (randomAlphabet == guess) {
             wins = wins + 1;
             domWins.textContent = wins; 
             newGame();
-            console.log("You Won! Play Again! " + " You have " + guesses + " remaining.");
         } else {
             alreadyGuessed.push(guess)
             domAlreadyGuessed.textContent += guess + " "; 
@@ -32,17 +28,15 @@ document.onkeyup = function play() {
                 domLosses.textContent = losses;
                 losses = losses + 1; 
                 newGame();
-                console.log("You Lost, Try Again! " + " You have " + guesses + " remaining.");
             }
         }
     } else {
-        console.log("You did not guess a letter from a-z.")
+        console.log("You did not guess an alphabet from a-z OR you've already guessed: " + guess)
     }
 }
 
 function newGame() {
     randomAlphabet = alphabet[Math.floor(Math.random() * alphabet.length)];
-    console.log("randomAlphabet: " + randomAlphabet);
     guesses = 9;
     alreadyGuessed = [];
     domAlreadyGuessed.textContent = "Your Guesses so far: ";
