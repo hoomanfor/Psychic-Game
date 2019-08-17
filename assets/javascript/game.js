337b9bfedee78df1da0bb7545c4f4635249a8706
Hoo Man <hoomanfor@gmail.com>
2019-08-17 11:38:37 -0400

    'wins', 'losses', 'guesses-left', appear on the DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,13 @@ var wins = 0;
 var losses = 0;
 var guesses = 9;
 var alreadyGuessed = [];
+
+var domWins = document.querySelector("#dom-wins");
+var domLosses = document.querySelector("#dom-losses");
+var domGuessesLeft = document.querySelector("#guesses-left");
 var domAlreadyGuessed = document.querySelector("#already-guessed");
+
+// alreadyGuessed.indexOf(guess) > -1
 
 document.onkeyup = function play() {
     var guess = event.key.toLowerCase();
@@ -16,15 +22,17 @@ document.onkeyup = function play() {
     if (event.keyCode >= 65 && event.keyCode <= 90) {
         if (randomAlphabet == guess) {
             wins = wins + 1;
+            domWins.textContent = wins; 
             newGame();
             console.log("You Won! Play Again! " + " You have " + guesses + " remaining.");
         } else {
             alreadyGuessed.push(guess)
             domAlreadyGuessed.textContent += guess + " "; 
-            console.log("alreadyGuessed: " + alreadyGuessed);
             guesses = guesses - 1;
-            console.log("guessesRemain: " + guesses);
+            domGuessesLeft.textContent = guesses;
             if (guesses == 0) {
+                domLosses.textContent = losses;
+                losses = losses + 1; 
                 newGame();
                 console.log("You Lost, Try Again! " + " You have " + guesses + " remaining.");
             }
